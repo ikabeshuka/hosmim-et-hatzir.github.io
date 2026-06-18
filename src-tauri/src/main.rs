4669ca0f2eecedb5709f4b825a61dbcd08033eda
ikabeshuka <ikabeshuka@gmail.com>
2026-06-18 21:42:14 +0300

    Refactor main function to use tauri_app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,5 @@
-// Prevents additional console window on Windows in release, do not remove!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    tauri_app::run();
 }
